972d4657312dfe61206e6a462c5cc3139db6dc7f
Tomasz Pająk <tomasz@pajak.name>
2022-01-03 13:36:42 +0100

    unit tests

--- Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
+++ Basic Calculator/task/src/main/java/org/hyperskill/calculator/MainActivity.kt
@@ -8,4 +8,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    var currentStudentName: String? = null
+
+    val lambda = {
+        val nameToPrint = currentStudentName ?: "Our Favorite Student"
+        println("Welcome $nameToPrint")
+    }
+
+    fun main() {
+        lambda() // output: Welcome Our Favorite Student
+        currentStudentName = "Nate"
+        lambda() // output: Welcome Nate
+    }
 }
